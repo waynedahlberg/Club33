d8792cb94000f92c13a9b2e0399320f6c38f3b96
Wayne Dahlberg <waynefj40@gmail.com>
2025-01-15 13:53:36 -0700

    Initial Commit

---
+++ Club33/Club33App.swift
@@ -0,0 +1,17 @@
+//
+//  Club33App.swift
+//  Club33
+//
+//  Created by Wayne Dahlberg on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Club33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
